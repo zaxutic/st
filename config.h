be1edbc3b60dd0d0c20dceda5d2555924478d212
Zaxutic <zaxuti@gmail.com>
2019-06-22 19:08:20 +1000

    Adjust default window dimensions

--- config.h
+++ config.h
@@ -133,8 +133,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 85;
+static unsigned int rows = 26;
 
 /*
  * Default colour and shape of the mouse cursor
